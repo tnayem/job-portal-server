5b5a603556db4d4d2b31e10c2fc95fae6143e639
tnayem <tasdidnayem@gmail.com>
2025-10-25 12:32:54 +0600

    implement patch method to update data

--- index.js
+++ index.js
@@ -76,6 +76,16 @@ async function run() {
       const result = await applicationCollection.find(query).toArray()
       res.send(result)
     })
+    // application data update 
+    app.patch('/application/:id', async(req,res)=>{
+      const id = req.params.id 
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set:{status:req.body.status}
+      }
+      const result = await applicationCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
